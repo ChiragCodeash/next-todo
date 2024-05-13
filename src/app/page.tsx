524c44c11e3d35be973091e74d59a58a20d8e3c9
ChiragCodeash <chiragk.codeash@gmail.com>
2024-05-13 12:02:31 +0530

    Your commit message here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import TodoList from './componets/TodoList'
 
 const page:React.FC = () => {
   return (
-    <div className='min-h-screen mx-auto container'>
+    <div className='min-h-screen mx-auto container  '>
       <div className='px-52 mt-24'>
       <AddInput/>
       <TodoList/>
